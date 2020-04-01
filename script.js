3eb0081f2c35a9069ba513b4d0f8aa261da16e1d
Kyle Sweigart <kasweigart@gmail.com>
2020-04-01 17:04:47 -0400

    first couple files

---
+++ script.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+    this.info = function() {
+        //return ...
+    }
+}
+
+function addBookToLibrary() {
+    //execute code here
+}
+
+function render() {
+
+}
