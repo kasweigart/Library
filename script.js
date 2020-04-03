d1888673957ceba8c06abeffd631f8a6deb6acf0
Kyle Sweigart <kasweigart@gmail.com>
2020-04-03 15:51:19 -0400

    added read and delete functionality

--- script.js
+++ script.js
@@ -1,38 +1,78 @@
+//CONSTANTS
+const newBookButton = document.querySelector("#newBook");
+const readButton = document.querySelectorAll("#readButton");
+const deleteButtons = document.querySelectorAll("#deleteButton");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const checkedInput = document.getElementById('read');
+const bookDisplay = document.getElementById("bookDisplay");
+
+
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
 
-    this.info = function() {
-        return `${title}${author}${pages}${read}`
-    }
-}
-
-function addBookToLibrary() {
-    //execute code here
-}
-
-function render() {
-
-}
-//CONSTANTS
-const newBookButton = document.querySelector('#newBook');
-const deleteButtons = document.querySelectorAll('#deleteButton');
-
-//EVENT LISTENERS
 function eventListeners() {
     deleteButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            console.log('Great!');
-        })
-    })
+      button.addEventListener("click", event => {
+        event.target.parentElement.parentElement.remove();
+      });
+    });
+  
+    newBookButton.addEventListener("click", addBookToLibrary);
+  
+    readButton.forEach(button => {
+      button.addEventListener("click", event => {
+        switch (event.target.innerText) {
+          case "Unfinished":
+            event.target.setAttribute("class", "btn btn-success");
+            event.target.innerText = "Finished";
+            break;
+          case "Finished":
+            event.target.setAttribute("class", "btn btn-warning");
+            event.target.innerText = "Unfinished";
+            break;
+        }
+      });
+    });
+  }
 
-    newBookButton.addEventListener('click', () => {
-        console.log('Nice!');
-    })
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+
+  this.info = function() {
+    // return `${title}${author}${pages}${read}`;
+  };
 }
 
+
+function addBookToLibrary() {
+  let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, checkedInput.checked);
+  myLibrary.push(newBook);
+  render(newBook);
+}
+
+
+function render(newBook) {
+  let newDiv = document.createElement("div");
+
+  let addBook = `<tr>
+    <td>${newBook.title}</td>
+    <td>${newBook.author}</td>
+    <td>${newBook.pages}</td>
+    <td><button type="button" class="btn btn-warning" id='readButton'>Unfinished</button></td>
+    <td><button type="button" class="btn btn-danger" id='deleteButton'>Delete</button></td>
+  </tr>`;
+
+  newDiv.innerHTML = addBook;
+  bookDisplay.append(newDiv);
+}
+
+
 eventListeners();
+
